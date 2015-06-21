ac378104bbf526f2cb1e51d5e19ddea403079e25
Patrick Johanneson <pjohanneson@gmail.com>
2015-06-21 17:44:40 -0500

    Start moving CPT stuff out of the theme and into a plugin

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Evans Movie Post Type
+ * Description: Provide the post type and associated tools for the Evans site
+ */
+require_once( 'class-evans-movie.php' );
+
+new Evans_Movie();
