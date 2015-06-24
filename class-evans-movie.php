5de0237a60d571ab21977a7334c5b3090cc7cc80
Patrick Johanneson <pjohanneson@gmail.com>
2015-06-23 22:13:10 -0500

    changed mind -- CMB2 out, Human Made cmb library in

--- class-evans-movie.php
+++ class-evans-movie.php
@@ -9,11 +9,10 @@ class Evans_Movie {
 	const PREFIX = '_evans_';
 
 	function __construct() {
-		add_action( 'init', array( $this, 'create_cpt' ) );	
-		
-		add_action( 'cmb2_init', array( $this, 'metaboxen' ) );
+		add_action( 'init', array( $this, 'create_cpt' ) );
+		add_action( 'init', array( $this, 'cmb_init' ), PHP_INT_MAX );
 
-		add_action( 'admin_notices', array( $this, 'check_for_cmb' ) );
+		add_filter( 'cmb_meta_boxes', array( $this, 'metaboxes' ) );
 	}
 
 	/**
@@ -29,61 +28,67 @@ class Evans_Movie {
 			),
 			'has_archive'	=> true,
 			'rewrite'		=> array('slug' => 'movies'),
-			'supports' => array('title','editor','thumbnail','custom-fields', 'comments', 'excerpt'),
+			'supports' => array('title','editor','thumbnail','comments'),
 		);
 		register_post_type( self::POST_TYPE, $args );
 	}
 
-	/**
-	 * Check to see if the CMB2 plugin is installed and active.
-	 */
-	function check_for_cmb() {
-		if( ! function_exists( 'new_cmb2_box' ) ) {
-			$class = "update-nag";
-			$message = "Please install and/or activate the CMB2 plugin for full functionality.";
-	        echo"<div class=\"$class\"> <p>$message</p></div>"; 
+	function cmb_init() {
+		if( ! class_exists( 'CMB_Meta_Box' ) ) {
+			require_once( 'lib/custom-meta-boxes/custom-meta-boxes.php' );
 		}
 	}
 
-
-	/**
-	* Hook in and add a demo metabox. Can only happen on the 'cmb2_init' hook.
-	*/
-	function metaboxen() {
-		// Start with an underscore to hide fields from custom fields list
+	function metaboxes( $metaboxes = array() ) {
 		$prefix = self::PREFIX;
 
-		$args = array(
-			'id' => $prefix . 'movie_metaboxen',
-			'title' => __( 'Movie Details', 'evans-movie' ),
-			'object_types' => array( self::POST_TYPE ),
-			'show_names' => true,
-		);
-
-		$meta_boxes = new_cmb2_box( $args );
-
-		//FIELDS:
-		// Showtimes (repeatable) X
-		// Official URL X 
-		// Rotten Tomatoes ID
-		// IMDB ID
-		// others?
-
-		$meta_boxes->add_field( array(
-			'name' => __( 'Showtimes', 'evans-movie' ),
-			'desc' => '',
-			'id' => $prefix . 'showtimes',
-			'type' => 'text_datetime_timestamp',
-			'repeatable' => true,
-			)	
-		);
-		$meta_boxes->add_field(
+		$url_group = array(
 			array(
-				'id' => $prefix . 'official_url',
-				'name' => __( 'Official URL', 'evans-movie' ),
+				'id' => 'url',
+				'name' => __( 'URL', 'evans-cpt' ),
 				'type' => 'text_url',
-			)
+				'cols' => 6,
+			),
+			array( 
+				'id' => 'url_name',
+				'name' => __( 'Link text', 'evans-cpt' ),
+				'type' => 'text',
+				'cols' => 6,
+			),
+
 		);
+		$fields = array(
+			// Showtime(s)
+			array(
+				'id' => 'showtime',
+				'name' => __( 'Showtimes', 'evans-cpt' ),
+				'type' => 'datetime_unix',
+				'repeatable' => true,
+				'repeatable_max' => 5,
+				'sortable' => true,
+			),
+
+			// URL group (official, IMDB, ... )
+			array(
+				'id' => 'url',
+				'name' => __( 'URL(s)', 'evans-cpt' ),
+				'type' => 'group',
+				'fields' => $url_group,
+				'repeatable' => true,
+			)
+
+		);
+
+		$metaboxes[] = array(
+			'id' => $prefix . 'movie_meta',
+			'title' => __( 'Movie Meta', 'evans-cpt' ),
+			'fields' => $fields,
+			'pages' => array( self::POST_TYPE ),
+			'priority' => 'high',
+
+		);
+
+		return $metaboxes;
 
 	}
 	
--- plugin.php
+++ plugin.php
@@ -2,7 +2,21 @@
 /**
  * Plugin Name: Evans Movie Post Type
  * Description: Provide the post type and associated tools for the Evans site
+ * Plugin URI:  http://patj.ca/wp/plugins/evans-movie-cpt
+ * Version:     1.0
+ * Author:      Patrick Johanneson
+ * Author URI:  http://patrickjohanneson.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path: /languages
+ * Text Domain: evans-cpt
  */
 require_once( 'class-evans-movie.php' );
+
+/**
+ * @todo Activation & deactivation hooks that flush rewrite rules
+ * @todo Rotten Tomatoes API key (admin pages)
+ * @todo TheMovieDB API key (admin pages)
+ */
 
 new Evans_Movie();
